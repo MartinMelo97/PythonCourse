d688b8fbf338eca20b8eb5578f740d3eccee53ab
Martin Melo Godinez <martin.melo.dev.97@gmail.com>
2020-04-14 14:16:33 -0500

    New requirements in exercise

--- calculator_senior_exercise.py
+++ calculator_senior_exercise.py
@@ -1,12 +1,13 @@
-# Hacer que la calculadora pueda recibir n numeros
-# Recibir los numeros con un sólo input, separados por comas
-# Escribe los numeros a jugar con ellos, separados por una coma -> 5,6,4,3,5,7
+# Hacer que la calculadora pueda recibir n números
+# Recibir los números con un sólo input, separados por comas
+# Escribe los números a jugar con ellos, separados por una coma -> 5,6,4,3,5,7
 
-# Realizar la suma de todos los numeros
-# Calcular el promedio de todos los numeros
-# Calcular la multiplicacion de todos los numeros
+# Realizar la suma de todos los números
+# Calcular el promedio de todos los números
+# Calcular la multiplicación de todos los números
 # Elevar cada uno de los números al cuadrado
 # Calcular el factorial de cada uno de los números
+# Y el factorial de la suma de todos los números
 
 # Modularizar lo más que se pueda el código
 
